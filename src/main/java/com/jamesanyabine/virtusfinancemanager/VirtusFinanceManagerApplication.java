0c58aaf76414dfa1006644fcace5a9fabb135df0
James Anyabine <67145572+VirtusDomo@users.noreply.github.com>
2024-04-07 21:22:50 -0500

    Initial Commit of Backend for VFM

---
+++ src/main/java/com/jamesanyabine/virtusfinancemanager/VirtusFinanceManagerApplication.java
@@ -0,0 +1,13 @@
+package com.jamesanyabine.virtusfinancemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtusFinanceManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtusFinanceManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jamesanyabine/virtusfinancemanager/VirtusFinanceManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jamesanyabine.virtusfinancemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtusFinanceManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
